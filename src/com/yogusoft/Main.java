c243c1e30d106d8488f06f0b03e062fa88c089cf
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2021-11-23 10:50:57 +0000

    Se ha añadido una clase getInputValue que solo tiene un metodo que pide números enteros por consola y se han pedido todos los datos necesarios en el main.

--- src/com/yogusoft/Main.java
+++ src/com/yogusoft/Main.java
@@ -1,8 +1,25 @@
 package com.yogusoft;
 
+import com.yogusoft.ExceptionHandlers.getInputValue;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Introduce el tamaño del huerto: ");
+        int sizeOrchard = getInputValue.getPositive_Integer();
+
+        System.out.print("Introduce el número de vegetales a producir: ");
+        int numberVegetablesProduced = getInputValue.getPositive_Integer();
+
+        System.out.print("Introduce el número de maximo de vegetales a consumir: ");
+        int numberVegetablesSell = getInputValue.getPositive_Integer();
+
+        System.out.println("------------------------------------------");
+
+        System.out.print("Introduce el número de clientes: ");
+        int numberClients = getInputValue.getPositive_Integer();
+
+        System.out.print("Introduce el número de productores: ");
+        int numberProducer = getInputValue.getPositive_Integer();
     }
 }
